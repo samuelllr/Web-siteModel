823b9fa479ef2b29a069754954d72a930c8f11f8
Samuelllr <ribeirosamuel848@gmail.com>
2023-04-14 20:59:26 -0300

    modificação 0.5
    construção da estrutura do site, pré reponsiva, posicionamento de alguns elemntos foram feitos no JS, e grande maioria no CSS3

--- index/script/index-script.js
+++ index/script/index-script.js
@@ -7,11 +7,16 @@ function start(){
     let header = document.getElementsByTagName("header")[0];
     let section = document.getElementsByTagName("section")[0];
     let footer = document.getElementsByTagName("footer")[0];
+    let barMenuHeader = document.getElementById("bar-menu-header");
+    let titleAlfa = document.getElementById("title-alfa");
+    let menuHeader = document.getElementById("menu-header");
 
     let heightHeader = header.clientHeight;
     let heightSection = section.clientHeight;
     
     section.style.top = heightHeader + "px";
     footer.style.top = heightHeader + heightSection + "px";
+    barMenuHeader.style.top = heightHeader - barMenuHeader.clientHeight + "px";
+    titleAlfa.style.height = heightHeader - barMenuHeader.clientHeight - menuHeader.clientHeight + "px" ; 
 
 }
