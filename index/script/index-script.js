2dccb3222da166be5b5fda95b6e2c0253100f179
Samuelllr <ribeirosamuel848@gmail.com>
2023-04-15 21:12:36 -0300

    modificação 0.6
    melhorias no CSS3 , modificação de estrutura para aplicação de estrutura futura.

--- index/script/index-script.js
+++ index/script/index-script.js
@@ -7,7 +7,6 @@ function start(){
     let header = document.getElementsByTagName("header")[0];
     let section = document.getElementsByTagName("section")[0];
     let footer = document.getElementsByTagName("footer")[0];
-    let barMenuHeader = document.getElementById("bar-menu-header");
     let titleAlfa = document.getElementById("title-alfa");
     let menuHeader = document.getElementById("menu-header");
 
@@ -16,7 +15,6 @@ function start(){
     
     section.style.top = heightHeader + "px";
     footer.style.top = heightHeader + heightSection + "px";
-    barMenuHeader.style.top = heightHeader - barMenuHeader.clientHeight + "px";
-    titleAlfa.style.height = heightHeader - barMenuHeader.clientHeight - menuHeader.clientHeight + "px" ; 
+    titleAlfa.style.height = heightHeader - menuHeader.clientHeight + "px" ; 
 
 }
