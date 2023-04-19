c0b1313a0dec81ed7f772ffbe34a1a8f1d0ce0f4
Enzo Felipe <108897318+Enzofelip@users.noreply.github.com>
2023-04-19 20:39:36 -0300

    modificação: 08
    configuração basica

---
+++ server/backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+//Configurando o recebimento de dados
+app.use()
+app.use(express.json());
+
+// Configurando a renderização do html
+const baseRule = path.join(__dirname, "template");
+
+app.get("/", (req, res) => {
+    res.sendFile(`${baseRule}/index.html`);
+});
