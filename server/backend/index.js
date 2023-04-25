01f7e2562ba4f9612e48c0655120872fc7cacc13
Enzo Felipe <108897318+Enzofelip@users.noreply.github.com>
2023-04-25 20:44:13 -0300

    modificação: 1.4
    app.use

--- server/backend/index.js
+++ server/backend/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mysql = require("mysql");
 
 //Configurando o recebimento de dados
-app.use()
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 // Configurando a renderização do html
@@ -13,7 +14,17 @@ app.get("/", (req, res) => {
     res.sendFile(`${baseRule}/index.html`);
 });
 
+//Criando exemplo post para dados de login de usuario
+app.post("/user/dados", (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+});
+
+//Método de conexão com o banco de dados
+
+
 app.listen(3000, () => {
 
-    
+    console.log("Servidor rodando na porta 3000");
 })
